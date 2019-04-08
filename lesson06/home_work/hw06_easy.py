dcc2561a96931be06025888b3e3e7bf155667b58
Pavel <pashatick@gmail.com>
2019-04-08 19:38:31 +0300

    добил HW06

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,5 +1,37 @@
-# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
+import math
+	
+# Задача-1: 	
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+
+class Triangle:
+	def __init__(self, ptA, ptB, ptC):
+		self.ptA = ptA
+		self.ptB = ptB
+		self.ptC = ptC
+
+	def line(self, point1, point2):
+		return math.sqrt((point1[0] - point2[0])**2 
+							+ (point1[1] - point2[1])**2)
+	
+	
+	def areaTriangle(self): 
+		semi_perimeter = self.line_sum() / 2
+		return math.sqrt(semi_perimeter 
+						* (semi_perimeter - self.line(self.ptA, self.ptB)) 
+						* (semi_perimeter - self.line(self.ptB, self.ptC)) 
+						* (semi_perimeter - self.line(self.ptC, self.ptA)))	
+	
+	def line_sum(self):
+		return self.line(self.ptA, self.ptB) + self.line(self.ptB, self.ptC) + self.line(self.ptC, self.ptA)
+	
+	def tri_H(self):
+		return self.areaTriangle() / (self.line(self.ptA, self.ptB) / 2)
+
+tri1 = Triangle((4, 9), (16, 25), (36, 49))
+print('Площадь треугольника', tri1.areaTriangle())
+print('Периметр треугольника', tri1.line_sum())
+print('Высота', tri1.tri_H())
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -7,3 +39,45 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+
+class Trapeze(Triangle):
+	def __init__(self, ptA, ptB, ptC, ptD):
+		super().__init__(ptA, ptB, ptC)		
+		self.ptD = ptD
+		self.lnAB = self.line(self.ptA, self.ptB)
+		self.lnBC = self.line(self.ptB, self.ptC)
+		self.lnCD = self.line(self.ptC, self.ptD)
+		self.lnDA = self.line(self.ptD, self.ptA)	
+	
+	def areaTriangle2(self): 
+		semi_perimeter = self.line_sum() / 2
+		return math.sqrt(semi_perimeter 
+						* (semi_perimeter - self.line(self.ptA, self.ptC)) 
+						* (semi_perimeter - self.lnCD) 
+						* (semi_perimeter - self.lnDA))
+
+	def trap_area(self):
+		return self.areaTriangle() + self.areaTriangle2()
+	
+	def line_sum_trap(self):
+		 return self.lnAB + self.lnBC + self.lnCD + self.lnDA
+	
+	def uniside(self):
+		if self.lnAB == self.lnCD and self.lnBC != self.lnDA:
+			return 'Трапеция равнобочная'
+		elif self.lnAB != self.lnCD and self.lnBC != self.lnDA:
+			return 'Трапеция не равнобочная'
+		else:
+			return 'Другая фигура'  
+		
+	def side_long(self):
+		return self.lnAB, self.lnBC, self.lnCD, self.lnDA
+
+trap1 = Trapeze((4, 10), (16, 223), (36, 49), (56,72))
+
+print()	
+
+print('Периметр трапеции = ', trap1.line_sum_trap())
+print('Площадь трапеции = ', trap1.trap_area())
+print(trap1.uniside())	
+print(trap1.side_long())
--- lesson06/home_work/hw06_hard.py
+++ lesson06/home_work/hw06_hard.py
@@ -10,3 +10,51 @@
 # С использованием классов.
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
 # каждый работник получал строку из файла
+
+import os
+
+class Workers_name:
+	def __init__(self, Name, Surname):
+		self.Name = Name
+		self.Surname = Surname
+		self.fullname = self.Name + ' ' + self.Surname
+
+class Works_hours(Workers_name):
+	def __init__(self, Name, Surname, Hours):
+		super().__init__(Name, Surname)		
+		self.Hours = int(Hours)
+		self.works_d = {self.fullname: self.Hours}
+
+class Workers(Workers_name):
+	def __init__(self, Name, Surname, Money, Special, Hours):
+		super().__init__(Name, Surname)
+		self.Money = int(Money)
+		self.Hours = int(Hours)
+		self.MPH = self.Money / self.Hours 
+	
+	
+wanted_name = input("Введите фамилию сотрудника ")
+
+money_list = []
+
+with open(os.path.join('data', 'hours_of'), 'r', encoding='UTF-8') as hours:
+	for line in hours:
+		if wanted_name in line:		
+			per = tuple(line.split())
+			W_H = Works_hours(per[0], per[1],per[2])
+			
+				
+
+with open(os.path.join('data', 'workers'), 'r', encoding='UTF-8') as workers:
+	for line in workers:		
+		if wanted_name in line:					
+			per = (line.split())
+			W_M = Workers(per[0], per[1], per[2], per[3], per[4])
+			print('Зарплата сотрудника {}'.format(W_M.fullname))			
+			if W_M.Hours == W_H.Hours:
+				print(W_M.Money)
+			elif W_M.Hours > W_H.Hours:
+				print (W_M.Money - ((W_M.Hours - W_H.Hours) * W_M.MPH))
+			elif W_M.Hours < W_H.Hours:
+				print (W_M.Money + ((W_H.Hours - W_M.Hours) * 2 * W_M.MPH))
+
