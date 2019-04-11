57a9899e98f6c0c6eb0391c5e23dabd6fe59a75c
Pavel <pashatick@gmail.com>
2019-04-11 23:31:05 +0300

    HW07

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,134 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+
+import random
+import sys
+
+
+class GameCard:
+	def __init__(self, Name):
+		self.players_name = Name
+		self.cards_numbers = random.sample(range(90), 15)
+		self.matrix = [['-']*9 for i in range(3)]	 
+	
+	def card_gen(self):
+		arr = self.matrix		
+		a = 0
+		card = []			
+		for lst in arr:
+			c = 0
+			i = 0		
+			k = random.sample(range(9), 5)						
+			while c < 5:							
+				lst[k[i]] = self.cards_numbers[a] 
+				c += 1
+				i += 1
+				a += 1		
+		for i in arr:
+			card.append(i)
+		return card
+
+babulya = GameCard(input('Введите сове имя: '))
+computer = GameCard('Компухтер')
+
+babulya_card = babulya.card_gen()
+computer_card = computer.card_gen()
+
+
+def print_card(name, arr):
+	print('{:-^40}'.format(name.players_name))	
+	for i in arr:
+		print(i)
+
+print_card(babulya, babulya_card)
+print_card(computer, computer_card)
+
+keg_list = random.sample(range(90), 90)
+
+def find_empty(player, arr, line):
+	if arr == [['-']*9 for i in range(3)]:
+		print('{} WIN!!!'.format(player.players_name))
+		sys.exit()	
+	if line == ['-']*9:
+		print('У игрока {} Квартира!!!'.format(player.players_name))
+		
+def Game(keg, name_player1, name_player2, player1_card, player2_card):
+	c = 89		
+	for digit in keg:				
+		print('Бочёнок № {}'.format(digit))
+		print('Осталось ходов {}'.format(c))		
+		answer = input('Хотите убрать цифру? Y/N')
+		print('#'*50)
+		print('#'*50)		
+		if answer == 'Y':
+			t = 0			
+			for line in player1_card:				
+				if digit in line:
+					line[line.index(digit)] = '-'
+				else:
+					t += 1 
+				if t == 3:
+					print('Такого номера нет в вашей карте! \n Вы проиграли!!! ;(')
+					sys.exit()
+				find_empty(name_player1, player1_card, line)		
+		
+		elif answer == 'N':
+			for line in player1_card:
+				if digit in line:
+					print('Вы пропустили бочёнок! \n Вы проиграли!!! ;(')
+					sys.exit()
+		
+		else:
+			print('Вы пропустили бочёнок! \n Вы проиграли!!! ;(')
+			sys.exit()
+		for line in player2_card:
+			if digit in line:
+				line[line.index(digit)] = '-'
+			find_empty(name_player2, player2_card, line)
+
+		print_card(name_player1, player1_card) 
+		print()		
+		print_card(name_player2, player2_card)
+		c -= 1
+
+Game(keg_list, babulya, computer, babulya_card, computer_card)			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
